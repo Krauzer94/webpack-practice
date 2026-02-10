458d34afc7830b7ce5929873dc85941a882f6ac0
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2026-02-10 19:58:57 -0300

    Load the Odin logo PNG image into the bundle

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
 
+// Import the Odin image file
+import odinImage from "./odin.png";
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
+
 console.log(greeting);
